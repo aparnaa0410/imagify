91f93198bcc673d4dce378dcffbe95304321ef3f
Aparnaa <aparnaa0410@gmail.com>
2025-07-12 22:36:20 +0530

    Fixed Razorpay integration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
 import userModel from "../models/userModel.js";
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import razorpay from 'razorpay'
+import transactionModel from "../models/transactionModel.js";
 
 const registerUser = async (req, res) => {
   try {
@@ -76,6 +78,71 @@ const userCredits = async (req, res) => {
     console.log(error.message);
     res.json({ success: false, message: error.message });
   }
+}
+
+const razorpayInstance= new razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+const paymentRazorpay = async (req, res) => {
+  try {
+    const { planId } = req.body;
+    const userId = req.userId;
+
+    if (!userId || !planId) {
+      return res.json({ success: false, message: 'Missing details' });
+    }
+
+    const userData = await userModel.findById(userId);
+
+    let credits, plan, amount, date;
+
+    switch (planId) {
+      case 'Basic':
+        plan = 'Basic';
+        credits = 100;
+        amount = 10;
+        break;
+      case 'Advanced':
+        plan = 'Advanced';
+        credits = 500;
+        amount = 50;
+        break;
+      case 'Business':
+        plan = 'Business';
+        credits = 5000;
+        amount = 250;
+        break;
+      default:
+        return res.json({ success: false, message: 'Plan not found' });
+    }
+
+    date = Date.now();
+
+    const transactionData = { userId, plan, amount, credits, date };
+    const newTransaction = await transactionModel.create(transactionData);
+
+    const options = {
+      amount: amount * 100,
+      currency: process.env.CURRENCY || 'INR',
+      receipt: newTransaction._id.toString(),
+    };
+
+    razorpayInstance.orders.create(options, (error, order) => {
+      if (error) {
+        console.log(error);
+        return res.json({ success: false, message: error });
+      }
+      res.json({ success: true, order });
+    });
+
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
 };
 
-export { registerUser, loginUser, userCredits };
+
+
+export { registerUser, loginUser, userCredits,  paymentRazorpay };
